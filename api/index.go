7b573333e4f8e7de40bea25d69e3d78d82aaade9
baotuo88 <baotuo888888@gmail.com>
2026-03-13 01:17:37 +0800

    Update index.go

--- api/index.go
+++ api/index.go
@@ -5,7 +5,7 @@ import (
 	"strings"
 	"sync"
 
-	"kerkerker-douban-service/internal/app"
+	"kerkerker-douban-service/app"
 	"kerkerker-douban-service/internal/config"
 
 	"github.com/rs/zerolog/log"
